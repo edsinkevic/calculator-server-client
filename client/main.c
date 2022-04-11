a88a8f6b7d69f6a60b3fce51937186b477a569b7
Edvin Sinkevič <55150562+edsinkevic@users.noreply.github.com>
2022-04-11 11:58:12 +0300

    Refactor (#2)
    * Add buggy parens support
    
    * Remove strtok_r
    
    * It works but you don't need parentheses...
    
    * Compile error
    
    * Remove unneeded parens support, keep it in another branch

--- client/main.c
+++ client/main.c
@@ -11,39 +11,39 @@
 
 int main(int argc, char const *argv[])
 {
-    struct sockaddr_in saddr;
-    char obuf[BUFSIZE];
-    char ibuf[BUFSIZE];
+        struct sockaddr_in saddr;
+        char obuf[BUFSIZE];
+        char ibuf[BUFSIZE];
 
-    const int cs = socket(AF_INET, SOCK_STREAM, 0);
-    const int l = sizeof(saddr);
-    const char *ip = "127.0.0.1";
+        const int cs = socket(AF_INET, SOCK_STREAM, 0);
+        const int l = sizeof(saddr);
+        const char *ip = "127.0.0.1";
 
-    bzero(&saddr, l);
-    saddr.sin_family = AF_INET;
-    saddr.sin_addr.s_addr = inet_addr(ip);
-    saddr.sin_port = htons(PORT);
+        bzero(&saddr, l);
+        saddr.sin_family = AF_INET;
+        saddr.sin_addr.s_addr = inet_addr(ip);
+        saddr.sin_port = htons(PORT);
 
-    connect(cs, (const struct sockaddr *)&saddr, l);
+        connect(cs, (const struct sockaddr *)&saddr, l);
 
-    while (1)
-    {
-        printf("------------------------------------------\n");
-        memset(ibuf, 0, BUFSIZE);
-        if (fgets(ibuf, BUFSIZE, stdin) == NULL)
-            break;
+        while (1)
+        {
+                printf("------------------------------------------\n");
+                memset(ibuf, 0, BUFSIZE);
+                if (fgets(ibuf, BUFSIZE, stdin) == NULL)
+                        break;
 
-        if (write(cs, ibuf, BUFSIZE) <= 0)
-            break;
+                if (write(cs, ibuf, BUFSIZE) <= 0)
+                        break;
 
-        memset(obuf, 0, BUFSIZE);
-        if (read(cs, obuf, BUFSIZE) <= 0)
-            break;
+                memset(obuf, 0, BUFSIZE);
+                if (read(cs, obuf, BUFSIZE) <= 0)
+                        break;
 
-        printf("%s", obuf);
-    }
+                printf("%s", obuf);
+        }
 
-    close(cs);
+        close(cs);
 
-    return 0;
+        return 0;
 }
