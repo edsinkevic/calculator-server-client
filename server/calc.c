14f18b312ecd7feb52a0cab82f66267092c43dca
Edvin Sinkevič <edvin.sinkevic@visma.com>
2022-04-11 11:57:27 +0300

    Remove unneeded parens support, keep it in another branch

--- server/calc.c
+++ server/calc.c
@@ -10,25 +10,11 @@ long fadd(long a, long b);
 long fminus(long a, long b);
 long ftimes(long a, long b);
 char handle_token(Stack *stack, char *dirty_token);
-char handle_parens(char *tok, long *res_ptr);
 char cpred(char c);
 
 #define BUFSIZE 1024
 
 void (*EXT_CALLBACK)(const char const *message) = NULL;
-
-void print_stack(Stack st)
-{
-    struct element *head = st.head;
-    printf("Printing what's left in stack:\n");
-    while (head != NULL)
-    {
-        printf("%d\n", (int)head->data);
-        head = head->next;
-    }
-
-    printf("    End.\n");
-}
 
 int calculate(char *unprocessed_input, long *result, void (*MSG_CALLBACK)(const char const *message))
 {
@@ -38,89 +24,22 @@ int calculate(char *unprocessed_input, long *result, void (*MSG_CALLBACK)(const 
     bzero(input, BUFSIZE);
     strcpy(input, unprocessed_input);
 
-    Stack st = init();
+    Stack st = sinit();
     char *tok = strtok(input, " ");
 
     while (tok != NULL)
     {
-        if (strlen(tok) >= 1 && tok[0] == '(')
-        {
-            long res = 0;
-            if (handle_parens(tok, &res))
-                push(&st, res);
-        }
-        else if (!handle_token(&st, tok))
+        if (!handle_token(&st, tok))
             return 0;
         tok = strtok(NULL, " ");
     }
 
-    const char result_status = pop(&st, result);
+    const char result_status = spop(&st, result);
 
-    print_stack(st);
-    free_stack(&st);
+    sprint(st);
+    sfree(&st);
     EXT_CALLBACK = NULL;
     return result_status;
-}
-
-char handle_parens(char *tok, long *res_ptr)
-{
-    if (strlen(tok) != 1 || tok[0] != '(')
-        return 0;
-
-    tok = strtok(NULL, " ");
-
-    char status = 0;
-    int parsize = 0;
-    int lsize = 10;
-    char **toks = (char **)calloc(lsize, sizeof(char *));
-
-    int i = 0;
-    for (; tok != NULL; ++i)
-    {
-        char *ctok = clean_token(tok, strlen(tok), &cpred);
-        if (strlen(ctok) == 1 && ctok[0] == ')')
-        {
-            status = 1;
-            char *rbuf = (char *)calloc(parsize, sizeof(char));
-            for (int j = 0; j < i; ++j)
-                sprintf(rbuf, "%s %s", rbuf, toks[j]);
-
-            calculate(rbuf, res_ptr, EXT_CALLBACK);
-            free(rbuf);
-            break;
-        }
-
-        if (i >= lsize)
-        {
-            lsize *= 2;
-            toks = (char **)realloc(toks, sizeof(char *) * lsize);
-        }
-        parsize += strlen(ctok) + 1;
-        toks[i] = ctok;
-
-        tok = strtok(NULL, " ");
-    }
-
-    for (int j = 0; j < i; ++j)
-        free(toks[j]);
-    free(toks);
-
-    return status;
-}
-
-int isnumber(const char *string)
-{
-    int i = 0;
-    const int l = strlen(string);
-    const char isneg = l > 1 && string[0] == '-';
-    if (isneg)
-        ++i;
-
-    for (i; i < l; ++i)
-        if (!isdigit(string[i]))
-            return 0;
-
-    return 1;
 }
 
 void bin_op_stack(Stack *st, long (*f)(long, long))
@@ -128,10 +47,10 @@ void bin_op_stack(Stack *st, long (*f)(long, long))
     if (ssize(st->head) >= 2)
     {
         long b;
-        pop(st, &b);
+        spop(st, &b);
         long a;
-        pop(st, &a);
-        push(st, (*f)(a, b));
+        spop(st, &a);
+        spush(st, (*f)(a, b));
     }
 }
 
@@ -168,6 +87,21 @@ long ftimes(long a, long b)
     return a * b;
 }
 
+int isnumber(const char *string)
+{
+    int i = 0;
+    const int l = strlen(string);
+    const char isneg = l > 1 && string[0] == '-';
+    if (isneg)
+        ++i;
+
+    for (i; i < l; ++i)
+        if (!isdigit(string[i]))
+            return 0;
+
+    return 1;
+}
+
 char cpred(char c)
 {
     return isalnum(c) || c == '-' || c == '+' || c == '*' || c == '(' || c == ')';
@@ -177,9 +111,10 @@ char handle_token(Stack *st, char *dirty_token)
 {
     const char *t = clean_token(dirty_token, strlen(dirty_token), &cpred);
     const int tlen = strlen(t);
+    char status = 1;
 
     if (isnumber(t) == 1)
-        push(st, atol(t));
+        spush(st, atol(t));
     else if (tlen == 1)
     {
         if (t[0] == '+')
@@ -188,13 +123,12 @@ char handle_token(Stack *st, char *dirty_token)
             bin_op_stack(st, &fminus);
         else if (t[0] == '*')
             bin_op_stack(st, &ftimes);
+        else
+            status = 0;
     }
     else
-    {
-        free((void *)t);
-        return 0;
-    }
+        status = 0;
 
     free((void *)t);
-    return 1;
+    return status;
 }
