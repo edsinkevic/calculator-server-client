42afe9e8a5d24f1d04b3c2bed5b9c022b45e0648
Edvin Sinkevič <edvin.sinkevic@visma.com>
2022-04-16 11:59:15 +0300

    Format stack.c

--- deps/stack.c
+++ deps/stack.c
@@ -4,86 +4,86 @@
 #include <stdlib.h>
 
 struct element {
-    STYPE data;
-    struct element *next;
+        STYPE data;
+        struct element *next;
 };
 
 typedef struct _stack {
-    struct element *head;
+        struct element *head;
 } stack;
 
 stack *sinit() {
-    stack *s = (stack *)malloc(sizeof(stack));
-    s->head = NULL;
-    return s;
+        stack *s = (stack *)malloc(sizeof(stack));
+        s->head = NULL;
+        return s;
 }
 
 int ssize(stack *st) {
-    struct element *h = st->head;
-    int count = 0;
-    while (h != NULL) {
-        count++;
-        h = h->next;
-    }
+        struct element *h = st->head;
+        int count = 0;
+        while (h != NULL) {
+                count++;
+                h = h->next;
+        }
 
-    return count;
+        return count;
 }
 
 char spush(stack *s, STYPE data) {
-    struct element *node = (struct element *)malloc(sizeof(struct element));
+        struct element *node = (struct element *)malloc(sizeof(struct element));
 
-    if (node == NULL)
-        return 0;
+        if (node == NULL)
+                return 0;
 
-    node->data = data;
-    node->next = s->head;
-    s->head = node;
+        node->data = data;
+        node->next = s->head;
+        s->head = node;
 
-    return 1;
+        return 1;
 }
 
 char sfull(stack *s) {
-    struct element *node = (struct element *)malloc(sizeof(struct element));
-    free(node);
-    return node == NULL;
+        struct element *node = (struct element *)malloc(sizeof(struct element));
+        free(node);
+        return node == NULL;
 }
 
 char sempty(stack *s) {
-    return s->head == NULL;
+        return s->head == NULL;
 }
 
 char spop(stack *s, STYPE *x) {
-    if (s->head != NULL) {
-        struct element *tmp = s->head;
-        STYPE value = s->head->data;
-        s->head = s->head->next;
-        free(tmp);
-        *x = value;
-        return 1;
-    }
+        if (s->head != NULL) {
+                struct element *tmp = s->head;
+                STYPE value = s->head->data;
+                s->head = s->head->next;
+                free(tmp);
+                *x = value;
+                return 1;
+        }
 
-    return 0;
+        return 0;
 }
 
 void sfree(stack *s) {
-    struct element *tmp;
+        struct element *tmp;
 
-    while (s->head != NULL) {
-        tmp = s->head;
-        s->head = s->head->next;
-        free(tmp);
-    }
+        while (s->head != NULL) {
+                tmp = s->head;
+                s->head = s->head->next;
+                free(tmp);
+        }
 
-    free(s);
+        free(s);
 }
 
 void sprint(stack *st) {
-    struct element *head = st->head;
-    printf("Printing what's left in stack:\n");
-    while (head != NULL) {
-        printf("%d\n", (int)head->data);
-        head = head->next;
-    }
+        struct element *head = st->head;
+        printf("Printing what's left in stack:\n");
+        while (head != NULL) {
+                printf("%d\n", (int)head->data);
+                head = head->next;
+        }
 
-    printf("    End.\n");
+        printf("    End.\n");
 }
